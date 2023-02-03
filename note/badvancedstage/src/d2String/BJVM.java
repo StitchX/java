73e43f94eb9696f7ab7768d81d11d0e3d382ee4b
StitchX <1916291530@qq.com>
2023-02-03 15:23:12 +0800

    jvm

--- note/badvancedstage/src/d2String/BJVM.java
+++ note/badvancedstage/src/d2String/BJVM.java
@@ -6,4 +6,19 @@ package d2String;
  * @data 2023/2/2 17:41
  */
 public class BJVM {
+    /*
+    根据jdk版本，jvm结构不一样
+    三种jvm:
+        sun公司的hotspot
+        bea公司的JRockit
+        IBM公司的J9 VM
+        后来bea被sun收购，现在一般使用的版本是hotspot（java -version）
+
+
+    jdk 1.6 : 字符串常量池在方法区里，方法区具体体现：永久化
+    jdk 1.7 : 字符串常量池在堆里；，方法区具体体现：永久化
+    jdk 1.8 : 字符串常量池在方法区里，方法区具体体现：元空间
+
+    后面有专门的课程学习JVM，此时先做了解
+     */
 }

923eb93259dc03d62d735a90a424815632a780a6
StitchX <1916291530@qq.com>
2023-02-09 17:19:03 +0800

    time

--- note/badvancedstage/src/d3date/BDateTime8.java
+++ note/badvancedstage/src/d3date/BDateTime8.java
@@ -1,11 +1,80 @@
 package d3date;
 
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 /**
  * @author Eva   Email:
  * @Description jdk 8中新日期时间API
  * @data 2023/2/9 16:18
  */
 public class BDateTime8 {
+    /*
+    localDate,localTime,localDateTime的使用
+    说明：
+        1、localTime相较于localDate，localDateTime，使用频率要高
+        2、类似于Calendar
+     */
+    @Test
+    public void test01(){
+//        now(): 获取当前的日期、时间、日期+时间
+        LocalDate localDate01 = LocalDate.now();
+        LocalTime localTime01 = LocalTime.now();
+        LocalDateTime localDateTime01 = LocalDateTime.now();
+        System.out.println(localDate01);
+        System.out.println(localTime01);
+        System.out.println(localDateTime01);
+        System.out.println("--------------------------");
+//        of()：设置指定的年、月、日、时、分、秒，没有偏移量
+        LocalDateTime localDateTime02 = localDateTime01.of(2020,10,9,10,10,10);
+        System.out.println(localDateTime02);
+        System.out.println("================================");
+//        getXxx()：获取相关的属性
+        System.out.println(localDateTime02.getDayOfMonth());
+        System.out.println(localDateTime02.getDayOfWeek());
+        System.out.println(localDateTime02.getMinute());
+        System.out.println(localDateTime02.getMonthValue()); // 10
+        System.out.println(localDateTime02.getMonth()); // OCTOBER
+
+//        体现不可变性
+//        withXxx()：设置相关的属性
+        LocalDate localDate02 = localDate01.withDayOfMonth(22);
+        System.out.println(localDate01); // 2023-02-09
+        System.out.println(localDate02); // 2023-02-22
+
+        LocalDateTime localDateTime03 = localDateTime01.withHour(4);
+        System.out.println(localDateTime01); // 2023-02-09T17:15:17.577821300
+        System.out.println(localDateTime03); // 2023-02-09T04:15:17.577821300
+
+//        不可变性
+        LocalDateTime localDateTime04 = localDateTime01.minusDays(6); // 当前日期减6天
+        LocalDateTime localDateTime05 = localDateTime01.plusDays(2); // 当前日期加2天
+        System.out.println(localDateTime04); // 2023-02-03T17:15:17.577821300
+        System.out.println(localDateTime05);
+
+    }
+    @Test
+    public void test02(){
+
+    }
+    @Test
+    public void test03(){
+
+    }
+    @Test
+    public void test04(){
+
+    }
+    @Test
+    public void test05(){
+
+    }
+}
+
+
     /*
     Calendar和Date的问题：
     1、可变性：像日期和时间这样的类应该是不可变的
@@ -14,10 +83,11 @@ public class BDateTime8 {
     此外，它们也不是线程安全的：不能处理闰秒
 
     总结：对日期和时间的操作一直是Java程序员最痛苦的地方之一
-     */
-    /*
+
     第三次引入API是成功的，并且java 8中引入的java.time API已经纠正了过去的缺陷，将来很长一段时间它都会为我们服务
     Java 8 吸收了Joda-Time的精华
+     */
+    /*
     新日期的API：
     java.time
     java.time.chrono
@@ -28,4 +98,3 @@ public class BDateTime8 {
     说明：大多数开发者只会用到基础包format包，也可能会用到temporat包，因此，尽管有69个新公开类型，
         大多数开发者，大概将指挥用到其中的三分之一
      */
-}

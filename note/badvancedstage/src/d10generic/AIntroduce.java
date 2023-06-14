f5a19ea20178c42bf4c8f80905f522dc8c48f5be
StitchX <1916291530@qq.com>
2023-06-14 17:28:51 +0800

    泛型

---
+++ note/badvancedstage/src/d10generic/AIntroduce.java
@@ -0,0 +1,17 @@
+package d10generic;
+
+/**
+ * @author Eva   Email:
+ * @Description
+ * @data 2023/6/14 17:17
+ */
+public class AIntroduce {
+    /*
+    1、为什么要有泛型
+    2、在集合中使用泛型
+    3、自定义泛型结构
+    4、泛型在继承上的体现
+    5、通配符的使用
+    6、泛型应用举例
+     */
+}

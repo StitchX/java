3bfb26048023da22859ba84ac9cfeaffebcaf82e
StitchX <1916291530@qq.com>
2023-06-27 15:12:33 +0800

    file

---
+++ note/badvancedstage/src/d11IO/AIntroduce.java
@@ -0,0 +1,24 @@
+package d11IO;
+
+/**
+ * @author Eva   Email:
+ * @Description
+ * @data 2023/6/26 16:23
+ */
+public class AIntroduce {
+    /*
+    1、File类的使用
+    2、IO流原理及流的分类
+    3、节点流（或文件流）
+    4、缓冲流
+    5、转换流
+    6、标准输入、输出流
+    7、打印流
+    8、数据流
+    9、对象流
+    10、随机存取文件流
+    11、NIO.2中Path、Paths、File类的使用
+     */
+
+//    类怎么造对象；有哪些常用的方法
+}

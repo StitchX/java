48558baa59abefed74dd74515d81c77be0122226
StitchX <1916291530@qq.com>
2023-07-20 17:13:36 +0800

    java8

---
+++ note/badvancedstage/src/d14java8/AIntroduce.java
@@ -0,0 +1,16 @@
+package d14java8;
+
+/**
+ * @author Eva   Email:
+ * @Description
+ * @data 2023/7/20 16:33
+ */
+public class AIntroduce {
+    /*
+    1. Lambda表达式
+    2. 函数式(Functional)接口
+    3. 方法引用与构造器引用
+    4. 强大的Stream API
+    5. Optional类
+     */
+}
